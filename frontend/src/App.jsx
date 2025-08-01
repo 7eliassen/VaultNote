0d31917beeb12e9b4b6b12d235af8f1d17ca92b4
eliassen <foo@mail.com>
2025-08-02 00:57:26 +0300

    base of UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import Registration from "./pages/auth/Registration.jsx";
+import Home from "./pages/home.jsx";
+
 
 function App() {
+  const navigate = useNavigate();
+
+  //Check if user is logged in
+  useEffect(() => {
+    const token = 1; // TODO: (HACK) pretend login check
+    if (token) {
+      navigate("/home");
+    } else {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Registration />} />
+    </Routes>
+  );
+}
+
+//I don't sure what is it, but it needs
+function Root() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   );
 }
 
-export default App;
+export default Root;
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Sidebar from "../components/home/Sidebar.jsx"
+import Editor from "../components/home/Editor.jsx"
+
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home_container">
+            <Editor />
+            <Sidebar />
         </div>
     )
 }
